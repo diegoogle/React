9d653b5a3848fce45449bcfcca8b73dee7d69b9e
dgomez <dgomez@benefit.mx>
2023-05-09 02:01:34 -0600

    update

---
+++ Native/01-reforzamiento/src/typescript/TiposBasicos.tsx
@@ -0,0 +1,11 @@
+
+export const TiposBasicos = () => {
+
+    let nombre: string | number = 'Diego'; //EL caracter '|' se utiliza para identificar mas de un tipo a una variable.
+    nombre = 12;
+  return (
+    <>
+      <h3>Tipos básicos</h3>  
+    </>
+  )
+}
